2a317275105d42ae6ccdc85ef5fa6e093e88d09a
EmersonGalerians <106710578+EmersonGalerians@users.noreply.github.com>
2023-09-22 15:59:33 -0300

    atualização

--- JS/Script.js
+++ JS/Script.js
@@ -1,42 +1,42 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
     document.querySelector('.login-form-container').classList.toggle('active');
 }
-document.querySelector('#close-login-form').onclick = () =>{
+document.querySelector('#close-login-form').onclick = () => {
     document.querySelector('.login-form-container').classList.remove('active');
 }
 
-menu.onscroll = () =>{
+menu.onscroll = () => {
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('header').classList.add('active');
-        
-    }else{
+
+    } else {
         document.querySelector('header').classList.remove('active');
     }
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-menu.onload = () =>{
+menu.onload = () => {
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('header').classList.add('active');
-        
-    }else{
+
+    } else {
         document.querySelector('header').classList.remove('active');
     }
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-document.querySelector('.home').onmousemove = (e) =>{
+document.querySelector('.home').onmousemove = (e) => {
     document.querySelectorAll('.home-parallax').forEach(elm => {
         let speed = elm.getAttribute('data-speed');
         let x = (window.innerWidth - e.pageX * speed) / 90;
@@ -46,9 +46,9 @@ document.querySelector('.home').onmousemove = (e) =>{
     });
 }
 
-document.querySelector('.home').onmouseleave = () =>{
+document.querySelector('.home').onmouseleave = () => {
     document.querySelectorAll('.home-parallax').forEach(elm => {
-        
+
         elm.style.transform = `translateX(0px) translateY(0px)`;
     });
 }
@@ -56,26 +56,80 @@ document.querySelector('.home').onmouseleave = () =>{
 var swiper = new Swiper(".vehicles-slider", {
     slidesPerView: 1,
     spaceBetween: 20,
-    loop:true,
-    grabCursor:true,
-    centeredSlides:true,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
     autoplay: {
         delay: 9500,
         disableOnInteraction: false,
-      },
+    },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      991: {
-        slidesPerView: 3, 
-      },
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
     },
-  });
+});
+
+var swiper = new Swiper(".featured-slider", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1040: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1040: {
+            slidesPerView: 3,
+        },
+    },
+});
